f21806440a3ea7e6130608976a072bcd11cbe229
parksb <parkgds@gmail.com>
2023-11-10 11:37:46 +0900

    Add arrow buttons and ACC switch

--- motion-controller/client/app/src/main/java/com/zap/motion_controller/MainActivity.kt
+++ motion-controller/client/app/src/main/java/com/zap/motion_controller/MainActivity.kt
@@ -7,9 +7,11 @@ import android.hardware.SensorEventListener
 import android.hardware.SensorManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.HapticFeedbackConstants
 import android.view.MotionEvent
 import android.view.View
 import android.widget.Button
+import android.widget.Switch
 import com.zap_lib.core.ZapClient
 import com.zap_lib.core.resources.ZapAccelerometer
 import com.zap_lib.core.resources.ZapUiComponent
@@ -19,6 +21,8 @@ class MainActivity : AppCompatActivity(), SensorEventListener {
     private lateinit var zap: ZapClient
     private lateinit var sensorManager: SensorManager
 
+    private var useAcc = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -27,10 +31,11 @@ class MainActivity : AppCompatActivity(), SensorEventListener {
         zap = ZapClient(InetAddress.getByName("192.168.35.213"))
 
         registerButtonTouchListeners()
+        findViewById<Switch>(R.id.accSwitch).setOnCheckedChangeListener { _, b -> useAcc = b }
     }
 
     override fun onSensorChanged(event: SensorEvent) {
-        if (event.sensor.type == Sensor.TYPE_ACCELEROMETER) {
+        if (useAcc && event.sensor.type == Sensor.TYPE_ACCELEROMETER) {
             val (x, y, z) = event.values
             zap.send(ZapAccelerometer(x, y, z))
         }
@@ -56,15 +61,26 @@ class MainActivity : AppCompatActivity(), SensorEventListener {
     private fun registerButtonTouchListeners() {
         val buttonTouchListener = { view: View, event: MotionEvent ->
             when (event.action) {
-                MotionEvent.ACTION_DOWN -> ZapUiComponent.Event.CLICK_DOWN
-                MotionEvent.ACTION_UP -> ZapUiComponent.Event.CLICK_UP
+                MotionEvent.ACTION_DOWN -> {
+                    view.performHapticFeedback(HapticFeedbackConstants.VIRTUAL_KEY)
+                    ZapUiComponent.Event.CLICK_DOWN
+                }
+                MotionEvent.ACTION_UP -> {
+                    view.performHapticFeedback(HapticFeedbackConstants.VIRTUAL_KEY_RELEASE)
+                    ZapUiComponent.Event.CLICK_UP
+                }
                 else -> null
             }?.let { zapEvent ->
-                zap.send(ZapUiComponent(view.resources.getResourceName(view.id), zapEvent))
+                zap.send(ZapUiComponent(view.id.toString(), zapEvent))
             }
 
             true
         }
+
+        findViewById<Button>(R.id.buttonL).setOnTouchListener(buttonTouchListener)
+        findViewById<Button>(R.id.buttonR).setOnTouchListener(buttonTouchListener)
+        findViewById<Button>(R.id.buttonU).setOnTouchListener(buttonTouchListener)
+        findViewById<Button>(R.id.buttonD).setOnTouchListener(buttonTouchListener)
 
         findViewById<Button>(R.id.buttonA).setOnTouchListener(buttonTouchListener)
         findViewById<Button>(R.id.buttonB).setOnTouchListener(buttonTouchListener)
